4eec0bdfdf044d0a4b68291407facf299da18437
Maxim Mitrofanov <m.mitrofanov@abm-jsc.ru>
2020-03-13 22:54:16 +0300

    2-module 2-task done

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,4 +4,10 @@
  * @returns {Boolean}
  */
 function isEmpty(obj) {
+    for(let key in obj){
+        if (obj.hasOwnProperty(key)) {
+            return false;
+        }
+    }
+    return true;
 }
