60024c43dc29295018ea17510a103af34fb85af7
Maxim Mitrofanov <m.mitrofanov@abm-jsc.ru>
2020-03-20 19:32:00 +0300

    3-module 3-task done

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,18 @@
  * @returns {string}
  */
 function camelize(str) {
-}
+    let arr=[];
+    let arr2=[];
+    let upper;
+    arr = str.split('-');
+    arr.forEach((el)=>{
+      if (arr.indexOf(el) > 0){
+        upper = el[0].toUpperCase();
+        el = el.slice( 1, el.length)
+        el = upper + el;
+      }
+      arr2.push(el);
+    });
+    let str2 = arr2.join('');
+    return str2;
+  }
