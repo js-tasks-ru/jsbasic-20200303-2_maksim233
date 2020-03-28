3347454c3a6b2f945c4ff427631bcfce8fa311cf
Maxim Mitrofanov <m.mitrofanov@abm-jsc.ru>
2020-03-28 11:59:55 +0300

    4-module 2-task done

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,12 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
-}
+
+    let trs = table.getElementsByTagName('tr');
+    let i = 0;
+   for(let tr of trs){
+     td = tr.getElementsByTagName('td')[i];
+     td.style.backgroundColor = 'red';
+     i++;
+   }  
+   }
